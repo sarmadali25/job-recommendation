04d3e1a9f6340bcea5b4d6562d37a67432e71220
sarmadali25 <dev.sarmadarshad@gmail.com>
2026-04-14 01:24:54 +0500

    add error in api response in case Rapid API throws error

--- back-end/main.py
+++ back-end/main.py
@@ -57,7 +57,10 @@ def health_check():
 @app.get("/refresh-jobs")
 def refresh_jobs(query: str = "" , page: int = 1, num_pages: int = 1, country: str = "all", date_posted: str = "all"):
     data = fetch_api_data(query, page, num_pages, country, date_posted)
-    jobs = data.get("data", [])
+    if not isinstance(data, dict) or "data" not in data:
+        
+        raise HTTPException(status_code=502, detail=data)
+    jobs = data["data"]
 
     try:
         persisted_count = upsert_jobs(jobs)
